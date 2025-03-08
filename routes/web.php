ea09ddd39b4dcc7a041e2b77046c1dc6cd59a8c3
adindaavn <adindadavina1@gmail.com>
2025-03-08 20:51:40 +0700

    Login Styling

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('layout.header');
+    return view('home');
+});
+Route::get('/login', function () {
+    return view('auth.login');
 });
